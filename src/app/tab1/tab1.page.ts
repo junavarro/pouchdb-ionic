166cddf08c60ea2ce4b432e402be3bab4c5caec5
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2020-04-16 19:07:05 -0600

    [Add] last pouch db

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import PouchDB from 'pouchdb';
 import { Platform } from '@ionic/angular';
+//import * as transform from 'transform-pouch'
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -16,12 +17,14 @@ export class Tab1Page implements OnInit {
     this.platform.ready().then(
       (ready) => {
         PouchDB.plugin(require('pouchdb-adapter-cordova-sqlite'));
-        this.db = new PouchDB('mydb.db', {
+        this.db = new PouchDB('inec.db', {
           adapter: 'cordova-sqlite',
           iosDatabaseLocation: 'Library',
           androidDatabaseImplementation: 2
         });
         PouchDB.plugin(require('transform-pouch'));
+        PouchDB.plugin(require('crypto-pouch'));
+        this.db.crypto("1223455");
 
         this.db.transform({
           incoming: (doc) => {
